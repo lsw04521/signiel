5a9329991277e2b32805627d9ec5bdbd71e9038a
lsw04521 <lsw04521@naver.com>
2021-12-22 17:22:14 +0900

    21-12-22

--- js/main.js
+++ js/main.js
@@ -4,14 +4,62 @@ $(function(){
 
       var ws=$(this).scrollTop();
 
-      // 오른쪽에서
+      // about
 
-      if(ws>300){
+      if(ws>350){
 
-        $('.s2').css("transform","translateX(0px)").css("opacity","1");
+        $('#about').css("transform","translateY(0px)").css("opacity","1");
 
+      }
+
+      // about
+
+      if(ws>1000){
+
+      $('#service').css("transform","translateY(0px)").css("opacity","1");
+      
+      }
+
+      // trevi
+
+      if(ws>1500){
+
+      $('.m_box').css("transform","translateY(0px)").css("opacity","1");
+      
+      }
+
+      // banner_case
+
+      if(ws>1700){
+
+      $('.banner_case').css("transform","translateY(0px)").css("opacity","1");
+      
+      }
+
+      // room
+
+      if(ws>2000){
+
+      $('#room').css("transform","translateY(0px)").css("opacity","1");
+      
+      }
+
+      // room_case
+
+      if(ws>2200){
+
+      $('.room_case').css("transform","translateY(0px)").css("opacity","1");
+      
       }
 
     });
 
+  });
+
+  //menu
+
+  $(function(){
+    $("#menu").click(function () {
+      $(".b").slideToggle()
+    });
   });
