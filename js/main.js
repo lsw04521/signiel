37f062b8642b61bab16e0cc9ef1dd4fcb3118c6d
lsw04521 <lsw04521@naver.com>
2021-12-24 15:31:23 +0900

    21-12-24

--- js/main.js
+++ js/main.js
@@ -10,25 +10,25 @@
 $(function(){
   function left() {
       $('#guest_container li:last').prependTo('#guest_container');
-      $('#guest_container').css('margin-left', -373);
+      $('#guest_container').css('margin-left', -386);
       $('#guest_container').stop().animate({ marginLeft: 0 }, 800);
   }
 
   function right() {
-      $('#guest_container').stop().animate({ marginLeft: -373 }, 800,function () {
+      $('#guest_container').stop().animate({ marginLeft: -386 }, 800,function () {
           $('#guest_container li:first').appendTo('#  guest_container');
           $('#guest_container').css({ marginLeft: 0 });
       });
   }
   
   function slide() {
-      $('#guest_container').stop().animate({ marginLeft: -373 },800, function () {
+      $('#guest_container').stop().animate({ marginLeft: -386 },800, function () {
           $('#guest_container li:first').appendTo('#guest_container');
           $('#guest_container').css({ marginLeft: 0 });
       });
   }
   
-  setInterval(slide, 5000);
+  setInterval(slide, 3000);
   
   $('.review_left').click(function(){
       left();
